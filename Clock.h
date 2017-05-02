3df2930bdaf410eb6b256145f97dfc2086115bc7
Nami <jobwave2019@gmail.com>
2017-05-02 19:27:19 +0900

    時計、オブジェクトベースクラスの追加

---
+++ Clock.h
@@ -0,0 +1,28 @@
+//**********************************//
+//*	名　前：Clock.h
+//*	内　容：時計クラス
+//*	日　付：2017.5.2
+//*	制作者：N.Shibayama
+//**********************************//
+
+#pragma once
+#include "SimpleMath.h"
+#include "ObjectBase.h"
+
+class Clock :public ObjectBase
+{
+protected:
+	bool m_hand;							//時計の針(長針(true)、短針(false))
+	DirectX::SimpleMath::Vector2 m_tipPos;	//針の先端座標
+
+public:
+	Clock();
+	~Clock();
+	void Update();								//更新
+	void Render();								//描画
+	bool getHand();								//針の状態取得
+	DirectX::SimpleMath::Vector2 getTipPos();	//針の先端座標取得
+	void clockwise();							//針を右回りに動かす
+
+
+};
